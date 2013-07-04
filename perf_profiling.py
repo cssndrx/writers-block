8a927a82a64f3711042ca769d8888538282c38cc
Cassandra Xia <xiac@mit.edu>
2013-07-04 09:13:12 -0400

    Added primitive cProfile script

---
+++ perf_profiling.py
@@ -0,0 +1,15 @@
+import cProfile
+import gui
+
+PROFILE_LOG = 'profile_results'
+NUM_LINES = 20
+
+
+## registration of the gui
+## loading of the corpora
+cProfile.run('gui.main()', PROFILE_LOG)
+
+
+import pstats
+p = pstats.Stats(PROFILE_LOG)
+p.sort_stats('tottime').print_stats(NUM_LINES)
