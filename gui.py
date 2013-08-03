c6e9bd86af0624f87c2da91a194e82abb786600b
Cassandra Xia <xiac@mit.edu>
2013-08-03 11:09:28 -0400

    Broke gui.py into two files

--- gui.py
+++ gui.py
@@ -6,12 +6,11 @@ import nltk ## todo: this is sketch
 from PyQt4.QtCore import * 
 from PyQt4.QtGui import * 
 
-from managers import CEO
-from config import IS_KEYLOGGER, USER_FOLDER
 from utils import *
+from gui_elements import *
+from managers import CEO
+from config import USER_FOLDER
 
-## todo: figure out where this should go
-font = QFont('Courier', 12, QFont.Light)
 
 def main():
     app = QApplication(sys.argv) 
@@ -19,88 +18,7 @@ def main():
     w.show()
 
     sys.exit(app.exec_()) 
-
-def is_keylogger():
-    """
-    Returns True if we are keylogging in order to offer suggestions (Windows-only)
-    """
-    return os.name == 'nt' and IS_KEYLOGGER
     
-
-def input_box():
-    """
-    Returns an input box that raises a QT SIGNAL
-    in response to the space bar
-    """
-    inp = SpacebarTextEdit()
-    inp.setFont(font)
-    return inp
-
-def btn(init_text='-'):
-    return QPushButton(init_text)
-
-def lbl(init_text='-'):
-    return QLabel(init_text)
-
-def output_box():
-    out = QTextEdit()
-    out.setReadOnly(True)
-    out.setAcceptRichText(True)
-    out.setFont(font)
-    return out
-
-def output_line():
-    out = QLineEdit()
-    out.setReadOnly(True)
-    out.setFont(font)
-    return out
-
-def output_word():
-    return lbl()
-
-def output_grid(entries):
-    """
-    Takes an iterable of tuple-pairs and returns a grid layout
-    """
-    grid = QGridLayout()
-    for i, (a, b) in enumerate(entries):
-        grid.addWidget(a, i, 0)
-        grid.addWidget(b, i, 1)
-    return grid
-
-def update_grid(grid, entries):
-    """
-    Takes an iterable of tuple-pairs and updates a grid layout
-    """
-    for i, (a, b) in enumerate(entries):
-        grid.itemAtPosition(i, 0).widget().setText(str(a))
-        grid.itemAtPosition(i, 1).widget().setText(str(b))
-
-
-def vstack_widgets(widgets, with_stretch=False):
-    """
-    Returns a layout that vertically stacks the given widgets
-    """
-    layout = QVBoxLayout()
-    for i, w in enumerate(widgets):
-        if isinstance(w, QWidget):
-            layout.addWidget(w)
-        elif isinstance(w, QLayout):
-            layout.addLayout(w)
-        else:
-            raise NotImplementedError('Tried to add unknown type %s to layout' % type(w))
-            
-        if with_stretch and i%2==1:
-            ## assumes that widgets are paired when adding stretches
-            layout.addStretch(1)
-
-    return layout
-
-
-def health_widget():
-    entries = [ (lbl(corpus_name), lbl()) for corpus_name in get_corpora().keys()]
-    return output_grid(entries)
-
 
 class MainWindow(QWidget): 
 
@@ -143,14 +61,14 @@ class MainWindow(QWidget):
                         lbl('Suggestions'),
                         self.time_grep,
                         self.grep_widget,
-                        lbl('Synonyms'),
-                        self.time_synonyms,
-                        self.synonyms,
+##                        lbl('Synonyms'),
+##                        self.time_synonyms,
+##                        self.synonyms,
                         lbl('Generative text'),
                         self.time_generative,
                         self.generative_text,
-                        lbl('Sandwich'),
-                        self.sandwich,
+##                        lbl('Sandwich'),
+##                        self.sandwich,
                         self.user_notifications,
                         ]
         self.wide_layout = vstack_widgets(wide_widgets)
@@ -238,10 +156,10 @@ class MainWindow(QWidget):
         if word:
             self.update_last_word_widget(word)
             self.update_grep_widget(word)
-            self.update_synonyms_widget(word)
+#            self.update_synonyms_widget(word)
             self.update_generative_text_widget(word)
             self.update_corpora_health_widget(word)
-            self.update_sandwich_widget(word)
+#            self.update_sandwich_widget(word)
        
         t2 = time.time()
         time_taken = (t2-t1)*1000.0
@@ -307,19 +225,6 @@ class MainWindow(QWidget):
         sandwich_text = CEO.sandwich(word)
         if sandwich_text:
             self.sandwich.setText(sandwich_text)
-            
-class SpacebarTextEdit(QTextEdit):
-    """
-    An input box that raises a QT SIGNAL in response to the space bar
-    """
-    def __init__(self, *args):
-        QTextEdit.__init__(self, *args)
-        
-    def event(self, event):
-        if (event.type()==QEvent.KeyPress) and (event.key()==Qt.Key_Space):
-            self.emit(SIGNAL('SPACE_PRESSED')) 
-
-        return QTextEdit.event(self, event)
 
         
 if __name__ == "__main__": 
